792c0d64d9d626ae08761d4e5e8cad10770dcce2
vinayboss9669 <vinaybos9669@gmail.com>
2025-10-15 21:54:16 +0530

    basic code of nexthjs

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            <h1 style={{ backgroundColor: 'white' }}>About Page</h1>
+            <p>This is the about page of our Next.js app.</p>
+        </div>
+    );
+};
+
+export default about;
